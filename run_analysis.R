941c3038dddc7fdf01df5b0d9f2900d48caf30a4
AM24 <30350452+AM24@users.noreply.github.com>
2021-02-26 19:04:21 -0500

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -17,8 +17,10 @@ dat<-rbind(train, test)
 ##Extracting only the measurements on the mean and standard deviation for each measurement into a separate data set.
 dat2<-dat[grepl("mean|std|activity|subject",names(dat))]#keep activity and subject column for future operations
 #Renaming with the activity column
-dat2$activity<-dat2$activity%>% gsub(1,"walking",.)%>%gsub(2,"walkingupsatirs",.)%>%gsub(3,"Walkingdownstairs",.)%>%gsub(4,"sitting",.)%>%gsub(5,"standing",.)%>%gsub(6,"laying",.)
+dat2$activity<-dat2$activity%>% gsub(1,"walking",.)%>%gsub(2,"walkingupstairs",.)%>%gsub(3,"Walkingdownstairs",.)%>%gsub(4,"sitting",.)%>%gsub(5,"standing",.)%>%gsub(6,"laying",.)
+#descriptive names for variables
+names<-colnames(dat2)%>%gsub(".[Bb]ody.", "linear acceleration",.)%>%gsub(".Acc.", "accelerometer",.)%>%gsub(".[Gg]yro.", "Gyroscope",.)%>%gsub("^t", "time domain signal",.)%>%gsub("^f","fourier transform signal",.)%>%gsub(".Mag.", "magnitude",.)
+colnames(dat2)<-names
 #creating the independent tidy data set with means for each subject and each activity
-actmeans<-sapply(split(dat2,dat2$activity),function(x){colMeans(x[,1:79])})
-submeans<-sapply(split(dat2,dat2$subject),function(x){colMeans(x[,1:79])})
-means<-cbind(actmeans,submeans)
+means<-sapply(split(dat2,list(dat2$activity,dat2$subject)),function(x){colMeans(x[,1:79])})
+
